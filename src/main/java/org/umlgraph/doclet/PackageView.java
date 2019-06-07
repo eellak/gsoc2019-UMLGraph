53ea030d1461fc155295b8c6e1b50afbb3176578
ekaratarakis <36879357+ekaratarakis@users.noreply.github.com>
2019-06-07 20:30:14 +0300

    Update PackageView.java

--- src/main/java/org/umlgraph/doclet/PackageView.java
+++ src/main/java/org/umlgraph/doclet/PackageView.java
@@ -1,8 +1,12 @@
 package org.umlgraph.doclet;
 
-import com.sun.javadoc.ClassDoc;
-import com.sun.javadoc.PackageDoc;
-import com.sun.javadoc.RootDoc;
+//import com.sun.javadoc.ClassDoc;
+//import com.sun.javadoc.PackageDoc;
+//import com.sun.javadoc.RootDoc;
+
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.PackageElement;
+import jdk.javadoc.doclet.DocletEnvironment;
 
 /**
  * A view designed for UMLDoc, filters out everything that it's not contained in
@@ -15,61 +19,67 @@ import com.sun.javadoc.RootDoc;
  * @author wolf
  * 
  */
-public class PackageView implements OptionProvider {
-
+public class PackageView implements OptionProvider 
+{
     private static final String[] HIDE = new String[] { "hide" };
-    private PackageDoc pd;
+    private PackageElement pd; // edit PackageDoc -> PackageElement
     private OptionProvider parent;
     private ClassMatcher matcher;
     private String outputPath;
     private Options opt;
-
-    public PackageView(String outputFolder, PackageDoc pd, RootDoc root, OptionProvider parent) {
-	this.parent = parent;
+    
+    // edit PackageDoc -> PackageElement
+    // edit RootDoc -> DocletEnvironment
+    public PackageView(String outputFolder, PackageElement pd, DocletEnvironment root, OptionProvider parent) 
+    {
+    	this.parent = parent;
 	this.pd = pd;
 	this.matcher = new PackageMatcher(pd);
 	this.opt = parent.getGlobalOptions();
 	this.opt.setOptions(pd);
-	this.outputPath = pd.name().replace('.', '/') + "/" + pd.name() + ".dot";
+	this.outputPath = pd.getSimpleName().toString().replace('.', '/') + "/" + pd.getSimpleName().toString() + ".dot"; // edit method name() -> getSimpleName()
     }
 
     public String getDisplayName() {
-	return "Package view for package " + pd;
+    	return "Package view for package " + pd;
     }
 
-    public Options getGlobalOptions() {
-	Options go = parent.getGlobalOptions();
-
+    public Options getGlobalOptions() 
+    {
+    	Options go = parent.getGlobalOptions();
 	go.setOption(new String[] { "output", outputPath });
 	go.setOption(HIDE);
-
 	return go;
     }
 
-    public Options getOptionsFor(ClassDoc cd) {
-	Options go = parent.getGlobalOptions();
+    public Options getOptionsFor(TypeElement cd) // from interface OptionProvider
+    {
+    	Options go = parent.getGlobalOptions();
 	overrideForClass(go, cd);
 	return go;
     }
 
-    public Options getOptionsFor(String name) {
-	Options go = parent.getGlobalOptions();
+    public Options getOptionsFor(String name) // from interface OptionProvider
+    {
+    	Options go = parent.getGlobalOptions();
 	overrideForClass(go, name);
 	return go;
     }
 
-    public void overrideForClass(Options opt, ClassDoc cd) {
-	opt.setOptions(cd);
+    public void overrideForClass(Options opt, TypeElement cd) // edit ClassDoc -> TypeElement
+    {
+    	opt.setOptions(cd);
 	boolean inPackage = matcher.matches(cd);
 	if (inPackage)
 	    opt.showQualified = false;
-	boolean included = inPackage || this.opt.matchesIncludeExpression(cd.qualifiedName());
-	if (!included || this.opt.matchesHideExpression(cd.qualifiedName()))
+	boolean included = inPackage || this.opt.matchesIncludeExpression(cd.getQualifiedName().toString()); // edit qualifiedName() -> getQualifiedName()
+	if (!included || this.opt.matchesHideExpression(cd.getQualifiedName().toString())) // edit qualifiedName() -> getQualifiedName()
 	    opt.setOption(HIDE);
     }
 
-    public void overrideForClass(Options opt, String className) {
-	opt.showQualified = false;
+    public void overrideForClass(Options opt, String className)
+    {
+    	opt.showQualified = false;
 	boolean inPackage = matcher.matches(className);
 	if (inPackage)
 	    opt.showQualified = false;
@@ -77,5 +87,4 @@ public class PackageView implements OptionProvider {
 	if (!included || this.opt.matchesHideExpression(className))
 	    opt.setOption(HIDE);
     }
-
 }

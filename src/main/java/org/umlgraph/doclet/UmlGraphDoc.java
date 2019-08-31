f8e6a70c8ab94613167b2833261259a345c8a96e
Evangelos Karatarakis <36879357+ekaratarakis@users.noreply.github.com>
2019-08-31 21:12:02 +0300

    Add a missing import in UmlGraphDoc

--- src/main/java/org/umlgraph/doclet/UmlGraphDoc.java
+++ src/main/java/org/umlgraph/doclet/UmlGraphDoc.java
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.TreeSet;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Set;
 import java.util.regex.Pattern;
 
